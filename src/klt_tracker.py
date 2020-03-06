9c58aad4d0734523c04daa02d16d21fd91f7d3c8
RahulSajnani <rahulsajnani2010@hotmail.com>
2020-03-06 22:30:07 +0530

    updated homography_filter

--- src/klt_tracker.py
+++ src/klt_tracker.py
@@ -1,3 +1,7 @@
+import numpy as np
+import cv2
+
+
 class KLT_Tracker:
     
     def __init__(self, images, feature_params, lk_params):
@@ -16,5 +20,26 @@ class KLT_Tracker:
     def track_features(self):
         pass
     
-    def homography_filter(self):
+    def homography_filter(self, threshold):
+        '''
+        Function to remove outliers points from optical flow
+        '''
+        no_of_cams = len(self.optical_flow[0])
+        no_of_pts = len(self.optical_flow)
+
+        image_pts = np.zeros((no_of_cams, no_of_pts, 2))
+        for i in range(no_of_pts):
+            for j in range(no_of_cams):
+                image_pts[j, i, 0] = self.optical_flow[i][j][0]
+                image_pts[j, i, 1] = self.optical_flow[i][j][1]
+
+        reference_image_pts = image_pts[0, :, :]    
+
+        for j in range(1, no_of_cams):
+            
+            homography_matrix = cv2.findhomography() 
+
+
+
+        
         pass
