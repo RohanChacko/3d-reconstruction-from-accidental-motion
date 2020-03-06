933edfdd0634d9a2f816c2bfb0526970c681c320
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2020-03-06 22:08:24 +0530

    adding the config

--- src/klt_tracker.py
+++ src/klt_tracker.py
@@ -1,8 +1,20 @@
 class KLT_Tracker:
     
-    def __init__(self):
+    def __init__(self, images, feature_params, lk_params):
+        self.reference_image = images[0]
+        self.images = images[1:]
+        self.reference_features = None
+        self.optical_flow = None
+        self.feature_params = feature_params
+        self.lk_params = lk_params
         pass
 
-    def get_features():
+    def get_features(self):
+
+        pass
+
+    def track_features(self):
         pass
     
+    def homography_filter(self):
+        pass
