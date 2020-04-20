ae628d390f8c130c5892de50656d7b4aeee2cd43
rohanchacko <rohanchacko@github.com>
2020-04-20 12:40:48 +0400

    updating crf

--- src/dense_crf.py
+++ src/dense_crf.py
@@ -10,14 +10,14 @@ import config
 import pydensecrf.densecrf as dcrf
 from pydensecrf.utils import create_pairwise_bilateral
 
-from plane_sweep import plane_sweep
+from plane_sweep import plane_sweep, Modulate
 
 class DenseCRF:
 
 	def __init__(self, rgb, unary, outfile) :
 
 		self.outfile = outfile
-		self.unary = unary
+		self.unary = unary.reshape((16,-1))
 		self.labels = unary.shape[0]
 		self.rgb = rgb
 		self.d = dcrf.DenseCRF2D(self.rgb.shape[1], self.rgb.shape[0], self.labels)
@@ -28,20 +28,31 @@ class DenseCRF:
 		self.d.setUnaryEnergy(self.unary)
 
 		# This adds the color-dependent term, i.e. features are (x,y,r,g,b).
-		self.d.addPairwiseBilateral(sxy=(5, 5), srgb=(20, 20, 20), rgbim=self.rgb, compat=np.array([1, self.labels*0.15]), kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+		self.d.addPairwiseBilateral(sxy=(5, 5), srgb=(20, 20, 20), rgbim=self.rgb, compat=np.array([1.0, self.labels*0.15]), kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+		# self.d.addPairwiseBilateral(sxy=(80,80), srgb=(13,13,13), rgbim=self.rgb, compat=10, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
 
 
 	def inference(self, iters=100) :
 
 		# Run inference steps.
 		Q = self.d.inference(iters)
-		print(Q.shape)
 		# Find out the most probable class for each pixel.
 		MAP = np.argmax(Q, axis=0)
+		print(MAP.dtype)
+		# print(MAP[MAP !=0])
 
-unary = plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=64, patch_radius = 1)
-print(unary.shape)
+unary = plane_sweep(min_depth=2, max_depth=4, scale=2, num_samples=64, patch_radius = 1)
+# unary = np.load('cost_volume_64.npy')
+unary = Modulate(unary)
+# print(unary.dtype)
+
+# for i in range(unary.shape[1]):
+# 	for j in range(unary.shape[2]):
+# 		print(unary[:2,i,j])
+
 img = cv2.imread('../datasets/stone6_still/stone6_still_0001.png')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+img = cv2.pyrDown(img)
+img = cv2.pyrDown(img)
 d = DenseCRF(img, unary, '')
-d.inference(100)
+d.inference(200)
--- src/plane_sweep.py
+++ src/plane_sweep.py
@@ -17,9 +17,10 @@ def Sad(img1, img2) :
 
 
 def HomographyFrom(K, C1, R1, C2, R2, dep):
+    # C1, R2 : Reference Image
 
-    H  = dep * K @ R2 @ R1.T * np.linalg.inv(K)
-    H[2,:] += K @ R2 @ (C1 - C2)
+    H  = dep * K @ R2 @ R1.T @ np.linalg.inv(K)
+    H[:,2] += K @ R2 @ (C1 - C2)
 
     return H
 
@@ -36,8 +37,51 @@ def MergeScores(scores):
 
     return score/num_valid_scores
 
+def GetMin(values, size):
+
+    assert(size>1)
+
+    f = 0
+    s = 0
+
+    if (values[0] > values[1]):
+        f = values[1]
+        s = values[0]
+    else :
+        f = values[0]
+        s = values[1]
+
+    for i in range(2, size) :
+
+        if values[i] < f :
+            f = values[i]
+        elif(values[i] < s):
+            s = values[i]
+        else :
+            pass
+
+    return f, s
+
+
+def Modulate(cost_volume_arr):
+    first = 0
+    second = 0
+    confidence = 0
+    num_samples = cost_volume_arr.shape[0]
+
+    for r in range(cost_volume_arr.shape[1]):
+        for c in range(cost_volume_arr.shape[2]):
+
+            values = cost_volume_arr[:, r, c]
+            first, second = GetMin(values, num_samples)
+            confidence = (second + 1) / (first + 1)
+            cost_volume_arr[:, r, c] = values * confidence
+
+    return cost_volume_arr
+
 def plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=16, patch_radius = 1):
 
+    print("Number of samples: ",{num_samples})
     # Intrinsics, Camera centers, Rotation mtx
     K = utilities.construct_camera_matrix(config.CAMERA_PARAMS)
     C = []
@@ -52,7 +96,7 @@ def plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=16, patch_radiu
             p = [float(r) for r in row[:-1]]
             rot, _ = cv2.Rodrigues(np.array(p[:3]))
             trans = np.array(p[3:6])
-            c = -1 * trans @ np.linalg.inv(rot)
+            c = -1 * np.linalg.inv(rot) @ trans
 
             C.append(c)
             R.append(rot)
@@ -71,7 +115,7 @@ def plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=16, patch_radiu
     step = 1.0 / (num_samples - 1.0)
     for val in range(num_samples):
         sample = (max_depth * min_depth) / (max_depth - (max_depth - min_depth) * val * step)
-        depth_samples.append(sample)
+        depth_samples.append(1781.0/sample)
 
 
     scaled_gray_images = []
@@ -106,7 +150,9 @@ def plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=16, patch_radiu
 
         # Assume 0th image is reference image
         for i in range(1, num_images):
-            warp = cv2.warpPerspective(scaled_gray_images[i], homographies[i], ref_img.shape, cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+            warp = cv2.warpPerspective(scaled_gray_images[i], homographies[i], ref_img.shape[::-1], cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+            cv2.imwrite('ref.png', ref_img)
+            cv2.imwrite(f'warp_{i}.png', warp)
             warped_images.append(warp)
 
         # len(scores) == len(warped_images)
@@ -122,6 +168,6 @@ def plane_sweep(min_depth=2, max_depth=4, scale = 2, num_samples=16, patch_radiu
 
                 cost_volume_arr[idx, y, x] = MergeScores(scores)
 
-    np.save('cost_volume.npz',cost_volume_arr)
-
+    np.save('cost_volume_64_2',cost_volume_arr)
+    print("Finished computing unary")
     return cost_volume_arr
